fc44e87215f1f177fdfe7e002ad66727b037e140
Andi Tamer <andi.tamer@skyline.be>
2022-06-11 15:32:39 +0200

    Add project files.

---
+++ Signal Problem Detected_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Signal Problem Detected_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Signal Problem Detected_2")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("E3AF8CA4-D8A8-4B80-9BA7-53E1A0F03F14")]
+[assembly: AssemblyVersion("1.0.0.0")]
